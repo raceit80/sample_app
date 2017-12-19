ecc72702073c025c05729aa984669e9664d45d25
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-19 21:06:13 +0000

    test for unsuccessful edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
-  get    '/google7c2e47df41406548', to: 'static_pages#google7c2e47df41406548'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
