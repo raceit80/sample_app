edea5dd5e6b38e13403d2b6f14fa28834049d56b
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-26 17:57:50 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example_user@gmail.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Ravi Subramanian",
+             email: "ravi.subram@gmail.com",
+             password:              "hello1234$$",
+             password_confirmation: "hello1234$$",
              admin:true)
 
 99.times do |n|
