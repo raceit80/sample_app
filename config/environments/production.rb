8bcc27286e41fa91d3d7adea23d6eb03639dc777
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-27 17:01:59 +0000

    updated host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-ravics.c9users.io'
+  host = 'blooming-temple-91498.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
