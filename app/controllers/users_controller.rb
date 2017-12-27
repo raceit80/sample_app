68131a403e6cf5feda2c0ae420a1cf2e2c2e1597
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-27 17:17:46 +0000

    changed activation email message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account. Takes 2-5 mins to receive activation email."
       redirect_to root_url
     else
       render 'new'
