b1d8ced6159c921ab67d92f077c420a2d1b595bf
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-27 17:22:32 +0000

    updated app name to welmote

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'blooming-temple-91498.herokuapp.com'
+  host = 'welmote.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
