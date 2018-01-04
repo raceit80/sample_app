e2cc943ff20a018ba5b0107754878ff3c08e2ef9
Ravi Subramanian <ravi.subram@gmail.com>
2018-01-04 19:56:50 +0000

    add user following complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,9 +83,13 @@ class User < ApplicationRecord
 
   # Defines a proto-feed.
   # See "Following users" for the full implementation.
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
-  end 
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
   
   # Follows a user.
   def follow(other_user)
